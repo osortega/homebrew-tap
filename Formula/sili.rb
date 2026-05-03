b3dfb9e7d526251da42557547708786a195b5e34
Osvaldo Ortega <48293249+osortega@users.noreply.github.com>
2026-05-03 11:22:52 -0700

    Update sili.rb

--- Formula/sili.rb
+++ Formula/sili.rb
@@ -1,7 +1,7 @@
 class Sili < Formula
   desc     "Codespace resource manager with auto-tunnel"
   homepage "https://github.com/osortega/sili"
-  url      "https://github.com/osortega/sili/archive/refs/tags/v0.1.0.tar.gz"
+  url      "https://github.com/osortega/sili/archive/refs/tags/v0.1.1.tar.gz"
   sha256   "9048434d3fe77551ea5a96cb70206335dafc34814089b6e9d85bcaa5f78885b7"
   license  "MIT"
 
